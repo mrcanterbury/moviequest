bd6e8bcae477050ddd72bca403eb615d2a6c91fb
mrcanterbury <mattrcanterbury@gmail.com>
2022-01-30 19:34:19 -0600

    first commit

---
+++ pages/main.js
@@ -0,0 +1,16 @@
+
+export default function Main() {
+    return (
+      <>
+        <Head>
+        <title>MovieQuest</title>
+        <meta name="description" content="Find your favorite movie genre" />
+        <link rel="icon" href="/favicon.ico" />
+        </Head>
+          <div>
+              <h1>Main Page</h1>
+          </div>
+      </>
+    )
+  }
+  
