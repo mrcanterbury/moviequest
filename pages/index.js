44247c048168ee323f925713ec1e644ea065d0b2
mrcanterbury <mattrcanterbury@gmail.com>
2022-01-30 20:58:46 -0600

    page construction

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,39 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Landing.module.css'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>MovieQuest</title>
+        <title>MovieQuest - Login</title>
         <meta name="description" content="Find your favorite movie genre" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <main className={styles.container}>
+      <main className={styles.container}>
 
-      <div className={styles.logo}>
-        Logo
-      </div>
-
-      <div className={styles.main}>
-        <div>
-              <h1>Login</h1>
+        <div className={styles.main}>
+          <div className={styles.logoContainer}>
+            <span className={styles.logo}>Movie<span className={styles.logoStrong}>Quest</span></span>
+          </div>
+          <div className={styles.formContainer}>
+                <h1>Account Login</h1>
+                <div className={styles.formFields}>
+                  <input className={styles.formInput} type="text" name="userName" id="userName" placeholder="Username"/>
+                  <input className={styles.formInput} type="text" name="userEmail" id="userEmail" placeholder="Email"/>
+                  <input className={styles.formInput} type="password" name="userPassword" id="userPassword" placeholder="Password"/>
+                    <div className={styles.formButtonFlex}>
+                      <button className={styles.btnRegister}>Register</button>
+                      <button className={styles.btnLogin}><Link href="/main">Login</Link></button>
+                    </div>
+                </div>
+                <div>
+                  <p className={styles.finePrint}>Choose 'Register' if this is your first visit.</p>
+                </div>
+          </div>
         </div>
-      </div>
 
-    </main>
+      </main>
     </>
   )
 } 
--- pages/main.js
+++ pages/main.js
@@ -1,16 +1,24 @@
+import Head from 'next/head'
+import styles from '../styles/Main.module.css'
+
 
 export default function Main() {
-    return (
-      <>
-        <Head>
-        <title>MovieQuest</title>
+  return (
+    <>
+      <Head>
+        <title>MovieQuest - Find Movies</title>
         <meta name="description" content="Find your favorite movie genre" />
         <link rel="icon" href="/favicon.ico" />
-        </Head>
-          <div>
-              <h1>Main Page</h1>
-          </div>
-      </>
-    )
-  }
+      </Head>
+      <main className={styles.container}>
+        <nav className={styles.navbar}>
+          <span className={styles.logo}>Movie<span className={styles.logoStrong}>Quest</span></span>
+        </nav>
+
+        <h1>Main Page</h1>
+
+      </main>
+    </>
+  )
+}
   
